d08bccc177232297d73fc46077b17cbe13d78873
Mehebul02 <mehebul2123@gmail.com>
2024-11-29 20:54:43 +0600

    create the student id

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,15 +1,32 @@
 import config from '../../config'
 import { TStudent } from '../student/student.interface'
-import { TUser } from './user.interface'
+import { NewUser } from './user.interface'
 import { USer } from './user.model'
 
 const createStudentIntoDB = async (password: string, studentData: TStudent) => {
 
+
+    const user: NewUser = {}
+
     // if password is not given, use default password 
-    if (!password) {
-        password = config.default_password as string
+    user.password = password || (config.default_password as string)
+
+
+    // student role 
+    user.role = 'student'
+    const result = await USer.create(user)
+
+    // set manually generated id
+
+    user.id = '251000'
+
+    // create student 
+    if (Object.keys(result).length) {
+        // set id, user _id 
+        studentData.id = result.id;
+        studentData.user = result._id;
     }
-    const result = await USer.create(studentData)
+
     return result
 }
 
