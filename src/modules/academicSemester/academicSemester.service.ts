2b0ec9aaad83f298d78dec0f6f3114388f5272be
Mehebul02 <mehebul2123@gmail.com>
2024-12-02 20:36:43 +0600

    create the invalid code on the semester code

--- src/modules/academicSemester/academicSemester.service.ts
+++ src/modules/academicSemester/academicSemester.service.ts
@@ -4,6 +4,20 @@ import { AcademicSemester } from "./academicSemester.model"
 
 const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
 
+    type TAcademicSemesterNameCodeMapper = {
+        [key: string]: string
+    }
+
+
+    const academicSemesterNameCodeMapper: TAcademicSemesterNameCodeMapper = {
+        'Autumn': '01',
+        "Summer": '2',
+        "Fall": '03'
+    }
+    if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+        throw new Error('Invalid semester code')
+    }
+
     const result = await AcademicSemester.create(payload)
     return result
 
