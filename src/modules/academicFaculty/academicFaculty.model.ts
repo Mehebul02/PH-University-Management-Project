a03ea67832b464eb8fa053dd6c14750c8ee9e6e4
Mehebul02 <mehebul2123@gmail.com>
2024-12-05 23:09:53 +0600

    create the update the faculty

--- src/modules/academicFaculty/academicFaculty.model.ts
+++ src/modules/academicFaculty/academicFaculty.model.ts
@@ -1,14 +1,20 @@
+import { Schema, model } from 'mongoose';
+import { TAcademicFaculty } from './academicFaculty.interface';
 
-import { Schema } from "mongoose";
-import { TAcademicFaculty } from "./academicFaculty.interface";
+const academicFacultySchema = new Schema<TAcademicFaculty>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
-
-const academicFacultySchema = new Schema<TAcademicFaculty>({
-
-    name: { type: String, required: true, unique: true }
-
-}, { timestamps: true })
-
-export const AcademicFaculty = {
+export const AcademicFaculty = model<TAcademicFaculty>(
+    'AcademicFaculty',
     academicFacultySchema
-}
+);
--- src/modules/academicFaculty/academicFaculty.validation.ts
+++ src/modules/academicFaculty/academicFaculty.validation.ts
@@ -2,9 +2,11 @@ import { z } from "zod";
 
 
 const academicFacultySchema = z.object({
-    name:z.string({
-        invalid_type_error:"Academic Faculty must be string"
-    }),
+    body:z.object({
+        name:z.string({
+            invalid_type_error:"Academic Faculty must be string"
+        }),
+    })
 })
 
 export const AcademicFacultyValidation ={
