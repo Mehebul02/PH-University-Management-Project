1c97393cb91fa290834eabadc646b29ef0760961
Mehebul02 <mehebul2123@gmail.com>
2024-12-06 22:45:44 +0600

    create the academic department controller successfully

---
+++ src/modules/academicDeparment/academicDepartment.controller.ts
@@ -0,0 +1,53 @@
+import { httpStatus } from 'http-status';
+import catchAsync from "../../app/utils/catchAsync";
+import sendResponse from "../../app/utils/sendResponse";
+import { AcademicDepartmentServices } from "./academicDepartment.service";
+
+
+
+const createAcademicDepartment = catchAsync(async (req, res) => {
+    const result = await AcademicDepartmentServices.createAcademicDepartmentIntoDB(req.body)
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Academic department created successfully',
+        data: result
+    })
+})
+const getAllAcademicDepartment = catchAsync(async (req, res) => {
+    const result = await AcademicDepartmentServices.getAllAcademicDepartmentFromDB()
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Academic department created successfully',
+        data: result
+    })
+})
+const getSingleAcademicDepartment = catchAsync(async (req, res) => {
+    const { departmentId } = req.params
+    const result = await AcademicDepartmentServices.getSingleAcademicDepartmentFromDB(departmentId)
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Academic department retrieved successfully',
+        data: result
+    })
+})
+const updateAcademicDepartment = catchAsync(async (req, res) => {
+    const { departmentId } = req.params
+    const result = await AcademicDepartmentServices.updateAcademicDepartmentFromDB(departmentId, req.body)
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Academic department updated successfully',
+        data: result
+    })
+})
+
+
+export const AcademicDepartmentControllers = {
+    createAcademicDepartment,
+    getAllAcademicDepartment,
+    getSingleAcademicDepartment,
+    updateAcademicDepartment
+}
