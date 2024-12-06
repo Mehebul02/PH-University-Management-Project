12a17635bd902ba82197b1cdaea1a0ce0d34cc8b
Mehebul02 <mehebul2123@gmail.com>
2024-12-06 21:58:17 +0600

    create the academic department model

---
+++ src/modules/academicDeparment/academicDepartment.model.ts
@@ -0,0 +1,21 @@
+import { model, Schema } from "mongoose";
+import { TAcademicDepartment } from "./academicDepartment.interface";
+
+const academicDepartmentSchema = new Schema<TAcademicDepartment>(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        academicFaculty: { type: Schema.Types.ObjectId }
+    },
+    {
+        timestamps: true,
+    },
+);
+
+export const AcademicDepartment = model<TAcademicDepartment>(
+    'AcademicFaculty',
+    academicDepartmentSchema
+);
