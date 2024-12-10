efc84d830d4af2b0c58246d23e478b1764491240
Mehebul02 <mehebul2123@gmail.com>
2024-12-10 23:18:00 +0600

    added the error source the file

--- src/app/middleWares/globalErrorHandling.ts
+++ src/app/middleWares/globalErrorHandling.ts
@@ -1,23 +1,58 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-labels */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 
-import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler, } from "express";
+import { ZodError, ZodIssue } from "zod";
+import { TErrorSource } from "../interface/error";
 
 
 
 const globalErrorHandle: ErrorRequestHandler = ((error, req, res, next) => {
 
 
-    const statusCode = error.statusCode || 500;
+    // eslint-disable-next-line prefer-const
+    let statusCode = error.statusCode || 500;
     const message = error.message || 'Something went wrong'
 
-    type TErrorSource = {
-        path: string | number
-        message:string
-    }[]
+
+    const errorSource: TErrorSource = [
+        {
+            path: '',
+            message: 'Something wend wrong'
+        }
+    ]
+
+    const handleError = (error: ZodError) => {
+
+        const errorSources: TErrorSource = error.issues.map((issue: ZodIssue) => {
+            return {
+                path: issue?.path[issue.path.length - 1],
+                message: issue.message
+            }
+        })
+
+        const statusCode = 400;
+        return {
+            statusCode,
+            message: "Zod Validation error",
+            errorSource
+        }
+    }
+
+    if (error instanceof ZodError) {
+        const simplifiedError = handleError(error);
+        message: 'Zod Error';
+
+    }
+
+
+
     res.status(statusCode).json({
         success: false,
+        errorSource,
         message,
-        error: error
+        AmrError: error
     })
 })
 
--- src/modules/academicDeparment/academicDepartment.model.ts
+++ src/modules/academicDeparment/academicDepartment.model.ts
@@ -21,25 +21,25 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
     }
 );
 
-academicDepartmentSchema.pre('save', async function (next) {
-    const isDepartExist = await AcademicDepartment.findOne({
-        name: this.name
-    })
-    if (isDepartExist) {
-        throw new AppError(httpStatus.NOT_FOUND, 'The department already exist')
-    }
-    next()
-})
+// academicDepartmentSchema.pre('save', async function (next) {
+//     const isDepartExist = await AcademicDepartment.findOne({
+//         name: this.name
+//     })
+//     if (isDepartExist) {
+//         throw new AppError(httpStatus.NOT_FOUND, 'The department already exist')
+//     }
+//     next()
+// })
 
-academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
-    const query = this.getQuery()
-    const isDepartmentExist = await AcademicDepartment.findOne(query)
-    if (!isDepartmentExist) {
-        throw new AppError(httpStatus.NOT_FOUND, 'Department dose not exist!')
-    }
-    next()
+// academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+//     const query = this.getQuery()
+//     const isDepartmentExist = await AcademicDepartment.findOne(query)
+//     if (!isDepartmentExist) {
+//         throw new AppError(httpStatus.NOT_FOUND, 'Department dose not exist!')
+//     }
+//     next()
 
-})
+// })
 
 export const AcademicDepartment = model<TAcademicDepartment>(
     'AcademicDepartment',
