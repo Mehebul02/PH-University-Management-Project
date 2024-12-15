973e73a6b11a9c4974dfb2dcc241658fad6331f3
Mehebul02 <mehebul2123@gmail.com>
2024-12-15 15:21:12 +0600

    added the model

---
+++ src/modules/admin/admin.model.ts
@@ -0,0 +1,120 @@
+import { model, Schema, Types } from "mongoose";
+import { AdminModel, TAdmin, TUserName } from "./admin.interface";
+import { BloodGroup, Gender } from "./admin.constant";
+
+
+
+const userNameSchema = new<TUserName>({
+    firstName: {
+        type: String,
+        required: [true, 'First name is required'],
+        trim: true,
+        maxlength: [20, 'Name can not be more than 20 characters'],
+    },
+    middleName: {
+        type: String,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        required: [true, 'Last Name is required'],
+        maxlength: [20, 'Name can not be more than 20 characters'],
+    }
+})
+
+const adminSchema = new Schema<TAdmin>({
+
+    id: {
+        type: String,
+        required: [true, 'ID is required'],
+        unique: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        required: [true, 'User id required'],
+        unique: true,
+        ref: 'User'
+    },
+    designation: {
+        type: String,
+        required: [true, 'Designation is required'],
+    },
+    gender: {
+        type: String,
+        enum: {
+            values: Gender,
+            message: '{VALUE} is not a valid gender',
+        },
+        required: [true, 'Gender is required'],
+    },
+    dateOfBirth: { type: Date },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+    },
+    contactNo: { type: String, required: [true, 'Contact number is required'] },
+    emergencyContactNo: {
+        type: String,
+        required: [true, 'Emergency contact number is required'],
+    },
+    bloodGroup: {
+        type: String,
+        enum: {
+            values: BloodGroup,
+            message: '{VALUE} is not a valid blood group',
+        },
+    },
+    presentAddress: {
+        type: String,
+        required: [true, 'Present address is required'],
+    },
+    permanentAddress: {
+        type: String,
+        required: [true, 'Permanent address is required'],
+    },
+    profileImg: { type: String },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    toJSON: {
+        virtuals: true,
+    },
+},)
+
+// generating full name 
+
+adminSchema.virtual('fullName').get(function () {
+    return (
+        this?.name?.firstName +
+        "" +
+        this?.name?.lastName +
+        "" +
+        this?.name?.middleName
+    )
+})
+
+
+adminSchema.pre("findOne", function (next) {
+    this?.find({ isDelete: { $ne: true } })
+    return next
+})
+
+adminSchema.pre('aggregate', function (next) {
+    this.pipeline().unshift({ $match: { isDelete: { $ne: true } } })
+    next()
+})
+
+
+//checking if user is already exist!
+
+adminSchema.statics.isUserExists = async function(id:string){
+    const existingUser = await Admin.find({id})
+    return existingUser
+}
+
+
+export const Admin = model<TAdmin, AdminModel>('Admin', adminSchema)
