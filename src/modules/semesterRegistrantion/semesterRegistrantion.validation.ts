0adc2a4dbd7bff6b03e6763a3369f914b85fa7e4
Mehebul02 <mehebul2123@gmail.com>
2024-12-27 21:46:37 +0600

    creta the validation

---
+++ src/modules/semesterRegistrantion/semesterRegistrantion.validation.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+import { semesterRegistrationStatus } from "./semesterRegistrantion.constrant";
+
+
+const createSemesterRegistrationValidationSchema = z.object({
+    body: z.object({
+        academicSemester: z.string(),
+        status: z.enum([...semesterRegistrationStatus as [string, ...string[]]]),
+        startDate: z.string().datetime(),
+        endDate: z.string().datetime(),
+        minCredit: z.number(),
+        maxCredit: z.string()
+    })
+})
+
+
+
+export const SemesterRegistrationValidations = {
+    createSemesterRegistrationValidationSchema
+}
